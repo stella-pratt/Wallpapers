6c35b579ab55b34356f1048ecc9b346a43e770c3
Sebastian Pratt <sebastian.pratt@student.onslow.school.nz>
2022-06-17 23:04:25 +1200

    crying alot, pain, why carousel so hard

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-height = window.screen.height
-width = window.screen.width
+let height = window.screen.height
+let width = window.screen.width
 if(height > width) {
     resolution = "Mobile"
 } else if(height > 1080 || width > 1920) {
@@ -8,3 +8,60 @@ if(height > width) {
     resolution = "FHD"
 }
 document.getElementById("resolution_text").innerHTML = "Recommended Resolution: " + resolution
+
+
+
+/* carosel */
+
+
+
+
+
+// Select all slides
+const slides = document.querySelectorAll(".slide");
+
+// loop through slides and set each slides translateX
+slides.forEach((slide, indx) => {
+  slide.style.transform = `translateX(${(indx * 100)}%)`;
+});
+
+// select next slide button
+const nextSlide = document.querySelector(".btn-next");
+
+// current slide counter
+let curSlide = 0;
+// maximum number of slides
+let maxSlide = slides.length - 1;
+
+// add event listener and navigation functionality
+nextSlide.addEventListener("click", function () {
+  // check if current slide is the last and reset current slide
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+  //   move slide by -100%
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
+
+// select prev slide button
+const prevSlide = document.querySelector(".btn-prev");
+
+// add event listener and navigation functionality
+prevSlide.addEventListener("click", function () {
+  // check if current slide is the first and reset current slide to last
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  } else {
+    curSlide--;
+  }
+
+  //   move slide by 100%
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
