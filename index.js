6efd49729dc109f05a6ce5d74f2c8cad3ad8b126
Sebastian Pratt <sebastian.pratt@student.onslow.school.nz>
2022-06-20 23:07:50 +1200

    carosel done still no wrap around :(

--- index.js
+++ index.js
@@ -11,10 +11,7 @@ document.getElementById("resolution_text").innerHTML = "Recommended Resolution: 
 
 
 
-/* carosel */
-
-
-
+/* carousel */
 
 
 // Select all slides
@@ -22,7 +19,7 @@ const slides = document.querySelectorAll(".slide");
 
 // loop through slides and set each slides translateX
 slides.forEach((slide, indx) => {
-  slide.style.transform = `translateX(${(indx * 100)}%)`;
+    slide.style.transform = `translateX(${(indx * 100)}%)`;
 });
 
 // select next slide button
@@ -34,18 +31,17 @@ let curSlide = 0;
 let maxSlide = slides.length - 1;
 
 // add event listener and navigation functionality
-nextSlide.addEventListener("click", function () {
-  // check if current slide is the last and reset current slide
-  if (curSlide === maxSlide) {
-    curSlide = 0;
-  } else {
-    curSlide++;
-  }
-
-  //   move slide by -100%
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
+nextSlide.addEventListener("click", function (){
+    // check if current slide is the first and reset current slide to last
+    if (curSlide === maxSlide) {
+        curSlide = 0;
+    } else {
+        curSlide++;
+    }
+    //   move slide by 100%
+    slides.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
 });
 
 // select prev slide button
@@ -53,15 +49,26 @@ const prevSlide = document.querySelector(".btn-prev");
 
 // add event listener and navigation functionality
 prevSlide.addEventListener("click", function () {
-  // check if current slide is the first and reset current slide to last
-  if (curSlide === 0) {
-    curSlide = maxSlide;
-  } else {
-    curSlide--;
-  }
+    // check if current slide is the first and reset current slide to last
+    if (curSlide === 0) {
+        curSlide = maxSlide;
+    } else {
+        curSlide--;
+    }
+    //   move slide by 100%
+    slides.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+    });
+});
+setInterval(moveSlide, 7500)
 
-  //   move slide by 100%
+function moveSlide() {
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
   slides.forEach((slide, indx) => {
     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
   });
-});
+}
