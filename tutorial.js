6907785c1e48b983554a22aaed16e0511f75928d
PRATTSE <spratt914@gmail.com>
2022-08-08 09:43:08 +1200

    startings tutorial

---
+++ tutorial.js
@@ -0,0 +1,15 @@
+let height = window.screen.height
+let width = window.screen.width
+if(height > width) {
+    resolution = "Mobile"
+} else if(height > 1080 || width > 1920) {
+    resolution = "4K"
+} else {
+    resolution = "FHD"
+}
+
+
+document.querySelector(".resolution_text").innerHTML = "Recommended Resolution: " + resolution
+
+
+document.querySelector(".os").innerHTML = "OS is -" + navigator.platform + "-"
